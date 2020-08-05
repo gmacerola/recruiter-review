5b10b1d65d72872dce5ce4915d4b8f9e33c55879
Gianpaolo Macerola <gmacerola@gmail.com>
2020-08-05 11:43:38 -0500

    functional, just needs styling

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const searchURL =
-  "https://api.github.com/users/USERNAME/repos";
-
-function displayRepos() {
-
+function getUserRepos(searchTerm) {
+  let searchURL = "https://api.github.com/users/" + `${searchTerm}/repos`;
+  console.log(searchURL);
+  fetch(searchURL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayRepos(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+      $("#js-form").addClass("hidden");
+      $("#restart").removeClass("hidden");
+      restart();
+    });
 }
 
-
-function getRepoList() {
-
+function displayRepos(responseJson) {
+  console.log(responseJson);
+  $("#results-list").empty();
+  $("#js-form").addClass("hidden");
+  let length = responseJson.length;
+  $("#results").removeClass("hidden");
+  console.log(responseJson[0].name);
+  for (let i = 0; i < length; i++) {
+    console.log("Really Testing");
+    $("#results-list").append(
+      `<li><h3>${responseJson[i].name}</h3>
+        <p><a href="${responseJson[i].html_url}">${responseJson[i].html_url}</p>
+        </li>`
+    );
+  }
+  restart();
 }
 
-function displayRepos(){
-    
+function restart() {
+  $("#restart").removeClass("hidden");
+  $(".restart").click((event) => {
+    $("#results-list").empty();
+    $("#results").addClass("hidden");
+    $("#js-form").removeClass("hidden");
+    $("#restart").addClass("hidden");
+    $("#js-error-message").text("");
+  });
 }
 
 function watchForm() {
-    $('form').submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
-
-
-    getUserRepos();
+    const username = $("#js-search-term").val();
+    getUserRepos(username);
+  });
 }
+
+$(watchForm());
