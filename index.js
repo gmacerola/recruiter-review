080c9a59c124ba868d71384303b6bf9c01902417
Gianpaolo Macerola <gmacerola@gmail.com>
2020-08-04 19:17:02 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const searchURL =
+  "https://api.github.com/users/USERNAME/repos";
+
+function displayRepos() {
+
+}
+
+
+function getRepoList() {
+
+}
+
+function displayRepos(){
+    
+}
+
+function watchForm() {
+    $('form').submit(event => {
+    event.preventDefault();
+
+
+    getUserRepos();
+}
